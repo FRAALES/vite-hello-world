8e78bf56bc0d2a524c1564b8587e321a8687d251
FRAALES <alessandrofranco1992@gmail.com>
2023-05-31 19:17:06 +0200

    bonus fatto

---
+++ src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppHero"
+}
+
+</script>
+
+<template>
+    <header>
+        Questo è un Jumbotron
+    </header>
+</template>
+
+<style scoped>
+header {
+    color: purple;
+}
+</style>
